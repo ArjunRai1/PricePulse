159c64b62830891a625b2414c36af0169fca98df
ArjunRai1 <arjunrai2214@gmail.com>
2025-05-22 15:07:26 +0530

    scraping and storage

---
+++ backend/db.py
@@ -0,0 +1,24 @@
+import sqlite3
+
+DB_PATH = "prices.db"
+
+def init_db():
+    conn = sqlite3.connect(DB_PATH)
+    c = conn.cursor()
+    c.execute("""
+    CREATE TABLE IF NOT EXISTS product_price(
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        url TEXT NOT NULL,
+        name TEXT NOT NULL,
+        price REAL NOT NULL,
+        timestamp TEXT NOT NULL
+    );""")
+    conn.commit()
+    conn.close()
+
+def save_price(url: str, name: str, price: float, timestamp: str):
+    conn = sqlite3.connect(DB_PATH)
+    c = conn.cursor()
+    c.execute("""INSERT INTO product_price (url, name, price, timestamp) VALUES (?, ?, ?, ?)""", (url, name, price, timestamp))
+    conn.commit()
+    conn.close()
